490dc4a2176f6acbedea08ef7820bd1420bc6559
Holcimger <maximilian.springer@holcim.com>
2025-04-30 08:33:21 -0300

    cambio en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://Holcimger.github.io/DaisyUI/', // Reemplaza 'DaisyUI' con el nombre de tu repositorio
 });
